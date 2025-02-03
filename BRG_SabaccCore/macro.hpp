f3b8eafc1dad24457cabd7270b176344b1cebaf9
Bragg <maxime.brochet.98@gmail.com>
2025-02-04 00:23:56 +0100

    Correction de la mise en forme des macros dans macro.hpp pour une meilleure lisibilité

--- BRG_SabaccCore/macro.hpp
+++ BRG_SabaccCore/macro.hpp
@@ -5,23 +5,23 @@
 #define F(NAME) \
     class NAME  \
     {           \
-    };
+    }
 
 #define F_PostInit(NAME) \
     class NAME           \
     {                    \
         postInit = 1     \
-    };
+    }
 
 #define RemoteExec(NAME, TARGET) \
     class NAME                   \
     {                            \
         allowedTargets = TARGET; \
-    };
+    }
 
 #define RemoteExecJIP(NAME, TARGET) \
     class NAME                      \
     {                               \
         allowedTargets = TARGET;    \
         jip = 1;                    \
-    };
+    }
